ba012318f4c3878ed7ce3fa0fa8d83a783a069e0
hieuvm <hieuvm.nws@gmail.com>
2024-01-23 17:38:31 +0700

    ceate story page and implement getx

--- lib/views/home/home_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram_stories/views/home/home_navigator.dart';
-import 'package:instagram_stories/views/home/home_state.dart';
-
-class HomeCubit extends Cubit<HomeState> {
-  HomeCubit({required this.navigator}) : super(const HomeState());
-  final HomeNavigator navigator;
-}
