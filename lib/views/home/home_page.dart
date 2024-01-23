bf5915a103f4d78a998aa77b935599ac572210cb
hieuvm <hieuvm.nws@gmail.com>
2024-01-23 17:39:56 +0700

    ceate ui home page

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,32 +1,233 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram_stories/views/home/home_cubit.dart';
-import 'package:instagram_stories/views/home/home_navigator.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:get/instance_manager.dart';
+import 'package:instagram_stories/commons/app_images.dart';
+import 'package:instagram_stories/commons/app_text_styles.dart';
+import 'package:instagram_stories/views/home/home_page_vm.dart';
+import 'package:instagram_stories/views/stories/story_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  HomePageVM vm = Get.put<HomePageVM>(HomePageVM());
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider<HomeCubit>(
-      create: (context) => HomeCubit(
-        navigator: HomeNavigator(context: context),
-      ),
-      child: const HomePageBody(),
+    List<String> listIcon = [
+      AppImages.img1,
+      AppImages.img2,
+      AppImages.img3,
+      AppImages.img4,
+      AppImages.img5,
+    ];
+    List<String> listTitles = [
+      "Design Tips",
+      "Portfolio",
+      "Resources",
+      "UI Basics",
+      "Web Design",
+      "Team",
+    ];
+    return GetBuilder<HomePageVM>(builder: (logic) {
+      return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: _appBar(),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Get.to(const StoryPage());
+                      },
+                      child: Image.asset(AppImages.imgAvatar),
+                    ),
+                    Row(
+                      children: [
+                        textInfUser(
+                          '2121',
+                          "Publicações",
+                        ),
+                        const SizedBox(width: 10),
+                        textInfUser(
+                          '421 m',
+                          "Seguidores",
+                        ),
+                        const SizedBox(width: 10),
+                        textInfUser(
+                          '2121',
+                          "A seguir",
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Username',
+                        style: AppTextStyle.black14Bold,
+                      ),
+                      Text(
+                        'Agência',
+                        style: AppTextStyle.grey14,
+                      ),
+                      Text(
+                        'O seu site de sonhos, está no sitio certo. 💻',
+                        style: AppTextStyle.black14W,
+                      ),
+                      Text(
+                        'codefive.pt',
+                        style: AppTextStyle.black14W,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  width: 100,
+                                ),
+                                Image.asset(AppImages.imgFollow1),
+                                Positioned(
+                                  left: 20,
+                                  child: Image.asset(AppImages.imgFollow2),
+                                ),
+                                Positioned(
+                                  left: 40,
+                                  child: Image.asset(AppImages.imgFollow3),
+                                )
+                              ],
+                            ),
+                            Expanded(
+                              child: Text(
+                                'Seguido por joaocorreia09, vanda.carvalho e 16 outros amigos',
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: AppTextStyle.black14W,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(listIcon.length,
+                              (index) => itemFeature(listIcon[index], listTitles[index])),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget textInfUser(String num, String type) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num,
+          style: AppTextStyle.black14W,
+        ),
+        Text(
+          type,
+          style: AppTextStyle.black14W,
+        ),
+      ],
     );
   }
-}
 
-class HomePageBody extends StatefulWidget {
-  const HomePageBody({super.key});
+  Widget itemFeature(String icon, String title) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            icon,
+          ),
+          Text(
+            title,
+            style: AppTextStyle.black12W,
+          ),
+        ],
+      ),
+    );
+  }
 
-  @override
-  State<HomePageBody> createState() => _HomePageBodyState();
-}
-
-class _HomePageBodyState extends State<HomePageBody> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      leading: InkWell(
+        child: SvgPicture.asset(
+          AppImages.icBack,
+          fit: BoxFit.scaleDown,
+        ),
+      ),
+      title: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(text: "Username", style: AppTextStyle.black16Bold),
+            const WidgetSpan(
+              child: SizedBox(width: 5),
+            ),
+            WidgetSpan(
+              child: SvgPicture.asset(
+                AppImages.icArtist,
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                AppImages.icBell,
+              ),
+              const SizedBox(width: 20),
+              SvgPicture.asset(
+                AppImages.icMenuTopBlack,
+                fit: BoxFit.scaleDown,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
