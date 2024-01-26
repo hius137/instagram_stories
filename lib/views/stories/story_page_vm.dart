a3f1c3a1fe2d1032d6ae3f6400eece3984349631
hieuvm <hieuvm.nws@gmail.com>
2024-01-26 10:57:22 +0700

    create local json file and model

--- lib/views/stories/story_page_vm.dart
+++ lib/views/stories/story_page_vm.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:instagram_stories/models/story_model.dart';
 
 class StoryPageVM extends GetxController{
   static StoryPageVM get find => Get.find<StoryPageVM>();
+  List<Story> stories = [];
 
+  
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/data/data.json');
+    final data = await json.decode(response);
+    stories = data["stories"];
+    update();
+  }
 }
