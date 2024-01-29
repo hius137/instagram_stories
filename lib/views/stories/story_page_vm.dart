d2fd88c87cb25dae77bd582f50685da36e399978
hieuvm <hieuvm.nws@gmail.com>
2024-01-29 14:31:41 +0700

    handle stories pageview

--- lib/views/stories/story_page_vm.dart
+++ lib/views/stories/story_page_vm.dart
@@ -8,41 +8,50 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:instagram_stories/models/stories_model.dart';
 import 'package:instagram_stories/models/story_model.dart';
 
-class StoryPageVM extends GetxController{
+class StoryPageVM extends GetxController {
   static StoryPageVM get find => Get.find<StoryPageVM>();
 
-  List<Story> stories = [];
-  List<Story> storiesByIdUser = [];
+  List<Story> listStories = [];
+  List<Story> listStoriesById = [];
+  List list = [];
 
-  PageController? pageController = PageController();
+  PageController? pageStoryController = PageController();
+  PageController? pageStoriesController = PageController();
   late AnimationController controller;
 
   int currentIndex = 0;
-  
-  Future<void> readJsonData(int idUser) async {
-    final String jsonData = await rootBundle.loadString('assets/data/data.json');
+
+  Future<void> readJsonData() async {
+    final String jsonData =
+        await rootBundle.loadString('assets/data/data.json');
     Map<String, dynamic> jsonMap = json.decode(jsonData);
     StoriesResponse storiesResponse = StoriesResponse.fromJson(jsonMap);
-    stories = storiesResponse.stories;
-    storiesByIdUser = stories.where((element) => element.userId == idUser).toList();
+    listStories = storiesResponse.stories;
+    int i;
+    for (i = 1; i <= 5; i++) {
+      listStoriesById =
+          listStories.where((element) => element.userId == i).toList();
+      list.add(listStoriesById);
+    }
     update();
   }
-  void getStoryByUserId(int idUser){
-    storiesByIdUser = stories.where((element) => element.userId == idUser).toList();
 
+  void getStoryByUserId(int idUser) {
+    listStoriesById =
+        listStories.where((element) => element.userId == idUser).toList();
   }
 
-  void showProgress(TickerProvider tickerProvider){
+  void showProgress(TickerProvider tickerProvider) {
     controller = AnimationController(
       vsync: tickerProvider,
       duration: const Duration(seconds: 5),
     )..addListener(() {
-      update();
-    });
+        update();
+      });
     controller.repeat(reverse: true);
   }
 
-  void onChangePage(value){
+  void onChangePage(value) {
     currentIndex = value;
     update();
   }
